e01df101008fbab6372999424ab82ab28ca45fa5
rabeeqasem <rabee.qa@hotmail.com>
2022-08-24 11:37:04 +0300

    split the files into train and test 80% 20%

--- code.py
+++ code.py
@@ -2,5 +2,7 @@ import os
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+import splitfolders
 
-print('rabee')
+splitfolders.ratio("reshaped_dataset", output="reshaped_dataset",
+    seed=1337, ratio=(.8, 0, .2), group_prefix=None, move=False) # default values
